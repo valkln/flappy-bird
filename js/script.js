95aaaa223319defbe7190395976498f2253773e2
valkln <kalinin.valentyn96@gmail.com>
2021-08-31 11:01:57 +0300

    added endgame state and reload after endgame

--- js/script.js
+++ js/script.js
@@ -33,6 +33,8 @@ pipe[0] = {
 
 function draw() {
 	ctx.drawImage(bg, 0, 0);
+	ctx.drawImage(fg, 0, canvas.height - fg.height);
+	ctx.drawImage(bird, xPos, yPos);
 	for (let i = 0; i < pipe.length; i++) {
 		ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
 		ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
@@ -46,16 +48,16 @@ function draw() {
 		if (xPos + bird.width >= pipe[i].x
 			&& xPos <= pipe[i].x + pipeUp.width
 			&& (yPos <= pipe[i].y + pipeUp.height
-				|| yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= canvas.height - fg.height) {
-			location.reload();
+				|| yPos + bird.height >= pipe[i].y + pipeUp.height + gap)
+			|| yPos + bird.height >= canvas.height - fg.height) {
+			lose();
 		}
 		if (pipe[i].x == 5) {
 			score++;
 			score_audio.play();
 		}
 	}
-	ctx.drawImage(fg, 0, canvas.height - fg.height);
-	ctx.drawImage(bird, xPos, yPos);
+
 
 	yPos += grav;
 	ctx.fillStyle = 'black';
@@ -70,3 +72,18 @@ function moveUp() {
 	yPos -= 20;
 	fly.play();
 } // действие при событии
+
+function lose() {
+	ctx.fillStyle = 'black';
+	ctx.font = '30px Verdana';
+	ctx.fillText('You lost!', 70, canvas.height - 300);
+	ctx.fillText('Your score: ' + score, 50, canvas.height - 250);
+	ctx.fillText('Press spacebar', 40, canvas.height - 200);
+	ctx.fillText('To Try Again', 55, canvas.height - 150);
+	document.addEventListener('keyup', event => {
+		if (event.code === 'Space') {
+			location.reload();
+		}
+	})
+	cancelAnimationFrame();
+}
